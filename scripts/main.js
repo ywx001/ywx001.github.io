8a0126e47ea9954f9cb5acbdbf2dbf6d6c50880f
ywx001 <1765441641@qq.com>
2024-05-08 16:26:58 +0800

    test commit

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/color-man.png") {
+    myImage.setAttribute("src", "images/flower.png");
+  } else {
+    myImage.setAttribute("src", "images/color-man.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  let myName = prompt("请输入你的名字。");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+}
+myButton.onclick = function () {
+  setUserName();
+};
